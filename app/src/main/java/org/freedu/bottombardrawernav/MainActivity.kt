8306db1f62b972f0602c951e8e42d6f863f4ba14
Musfiqur Rahman <mushfiqur15-10872@diu.edu.bd>
2023-12-31 12:45:35 +0600

    Drawer Navigation

--- app/src/main/java/org/freedu/bottombardrawernav/MainActivity.kt
+++ app/src/main/java/org/freedu/bottombardrawernav/MainActivity.kt
@@ -2,11 +2,14 @@ package org.freedu.bottombardrawernav
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import org.freedu.bottombardrawernav.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var actionBarDrawerTaggle:ActionBarDrawerToggle
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -15,5 +18,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         var navConlloer = findNavController(R.id.fragmentContainerView)
         binding.bottonBar.setupWithNavController(navConlloer)
+
+        actionBarDrawerTaggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.nav_open, R.string.nav_close);
+        actionBarDrawerTaggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.draweNav.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.homeFragment -> {
+                    binding.drawerLayout.closeDrawers()
+                    binding.draweNav.setupWithNavController(navConlloer)
+                }
+                R.id.profileFragment ->{
+                    binding.drawerLayout.closeDrawers()
+                    binding.draweNav.setupWithNavController(navConlloer)
+                }
+                R.id.settingsFragment ->{
+                    binding.drawerLayout.closeDrawers()
+                    binding.draweNav.setupWithNavController(navConlloer)
+                }
+                R.id.logout ->{
+                    binding.drawerLayout.closeDrawers()
+                    binding.draweNav.setupWithNavController(navConlloer)
+                }
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if(actionBarDrawerTaggle.onOptionsItemSelected(item)){
+            true
+        }
+        else super.onOptionsItemSelected(item)
     }
 }
